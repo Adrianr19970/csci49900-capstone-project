575b3254594bedf54d12486bdb91241c04a46bfc
DesperadoSP <khazix595@yahoo.com>
2021-11-01 16:32:34 -0400

    Added metrics to views

--- tradebreath/tradebreath1/tbapp/views.py
+++ tradebreath/tradebreath1/tbapp/views.py
@@ -67,3 +67,114 @@ def get_candlestick_data(bars): #Use this on the output of api.get_bars
       ts = x.Index
       arr.append((ts.strftime('%Y-%m-%d %X'), x.low, x.high, x.open, x.close))
     return arr
+
+MOST_RECENT_TIME = str(datetime.datetime.now() - datetime.timedelta(minutes=16))[0:10] + "T" + str(datetime.datetime.now() - datetime.timedelta(minutes=16))[11:19] + "Z"
+
+#print(MOST_RECENT_TIME)
+
+
+def days_offset_fun(RIGHT_NOW):
+  market_open = datetime.time(13,31)
+
+  if RIGHT_NOW.weekday() == 0 and RIGHT_NOW.time() < market_open:
+    days_offset = 8
+  elif RIGHT_NOW.weekday() == 0 and RIGHT_NOW.time() > market_open:
+    days_offset = 1
+  else:
+    days_offset = RIGHT_NOW.weekday() + 1
+  return days_offset
+
+
+days_offset_var = days_offset_fun(datetime.datetime.now() - datetime.timedelta(minutes=16))
+
+my_time = datetime.datetime.now() - datetime.timedelta(minutes=16) - datetime.timedelta(days=days_offset_var)
+
+last_week= str(my_time)[0:10] + "T" + str(my_time)[11:19]+ "Z"
+
+#print(last_week)
+
+def last_day_close_fun(stock):
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_close= last_week_day_bars[-1].c
+  return "Last Day Close", last_day_close
+
+def last_day_open_fun(stock):
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_open= last_week_day_bars[-1].o
+  return "Last Day Open", last_day_open
+
+def last_day_dollar_change_fun(stock):
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_dollar_change = round(last_week_day_bars[-1].c - last_week_day_bars[-1].o, 2)
+  return "Last Day Dollar Change", last_day_dollar_change
+
+def last_day_perc_change_fun(stock):
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_dollar_change = last_week_day_bars[-1].c - last_week_day_bars[-1].o
+  last_day_perc_change = (last_day_dollar_change / last_week_day_bars[-1].o) * 100
+  return "Last Day Percent Change", last_day_perc_change
+
+def last_day_volume_fun(stock):
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_volume = last_week_day_bars[-1].v
+  return "Last Day Volume", last_day_volume
+
+def last_day_low_fun(stock):
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_low = last_week_day_bars[-1].l
+  return "Last Day Low", last_day_low
+
+def last_day_high_fun(stock):
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_high = last_week_day_bars[-1].h
+  return "Last Day High", last_day_high
+
+one_year_ago_today = datetime.datetime.now() - datetime.timedelta(days=365)
+
+one_year_ago_today_str = str(one_year_ago_today - datetime.timedelta(minutes=16))[0:10] + "T" + str(one_year_ago_today - datetime.timedelta(minutes=16))[11:19] + "Z"
+
+#print(one_year_ago_today)
+
+almost_one_year_ago_today = datetime.datetime.now() - datetime.timedelta(days=364)
+
+almost_one_year_ago_today_str = str(almost_one_year_ago_today - datetime.timedelta(minutes=16))[0:10] + "T" + str(almost_one_year_ago_today - datetime.timedelta(minutes=16))[11:19] + "Z"
+
+#print(almost_one_year_ago_today)
+
+
+def one_year_change_fun(stock): 
+  one_year_ago_today_bars = api.get_bars(stock, TimeFrame.Day, one_year_ago_today_str, almost_one_year_ago_today_str)
+  one_year_ago_today_close = one_year_ago_today_bars[0].c
+  last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
+  last_day_close= last_week_day_bars[-1].c
+  one_year_change_per = (last_day_close - one_year_ago_today_close) / one_year_ago_today_close * 100
+  return "One Year Percent Change", one_year_change_per
+  
+def one_year_high_fun(stock):
+  one_years_bars = api.get_bars(stock, TimeFrame.Day, one_year_ago_today_str, MOST_RECENT_TIME)
+  one_years_highs = []
+  for i in range(0, len(one_years_bars)-1):
+    one_years_highs.append(one_years_bars[i].h)
+  one_year_high = max(one_years_highs)
+  return "52 Week High", one_year_high
+
+def one_year_low_fun(stock):
+  one_years_bars = api.get_bars(stock, TimeFrame.Day, one_year_ago_today_str, MOST_RECENT_TIME)
+  one_years_lows = []
+  for i in range(0, len(one_years_bars)-1):
+    one_years_lows.append(one_years_bars[i].l)
+  one_year_low = min(one_years_lows)
+  return "52 Week Low", one_year_low
+
+
+#print(last_day_close_fun("AAPL"))
+#print(last_day_open_fun("AAPL"))
+#print(last_day_dollar_change_fun("AAPL"))
+#print(last_day_perc_change_fun("AAPL"))
+#print(last_day_volume_fun("AAPL"))
+#print(last_day_low_fun("AAPL"))
+#print(last_day_high_fun("AAPL"))
+
+#print(one_year_change_fun("AAPL"))
+#print(one_year_high_fun("AAPL"))
+#print(one_year_low_fun("AAPL"))
