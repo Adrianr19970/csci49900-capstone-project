cea5e5ca6099583e23243389a530348277759ab8
DesperadoSP <khazix595@yahoo.com>
2021-11-30 16:16:24 -0500

    Added documentation

--- tradebreath/tradebreath1/tbapp/views.py
+++ tradebreath/tradebreath1/tbapp/views.py
@@ -5,21 +5,19 @@ import alpaca_trade_api as tradeapi
 import datetime
 import requests
 
+# ALPACA API KEYS
 # os.environ['APCA_API_KEY_ID'] = 'PK87MNNJ3DIDGDHWMTH6'
 # os.environ['APCA_API_SECRET_KEY'] = 'r8llTDlXflevWpchpjkqGPHLA5QYxIRs7tfCUY6n'
-
 api = tradeapi.REST(key_id='PK87MNNJ3DIDGDHWMTH6', secret_key='r8llTDlXflevWpchpjkqGPHLA5QYxIRs7tfCUY6n', base_url="https://paper-api.alpaca.markets")
-#print(dir(TimeFrame))
-#api.get_bars("AAPL", TimeFrame.Minute, "2021-06-08", "2021-06-08", adjustment='raw').df
 
+# ALPACA AVAILABLE INTERVALS FOR CANDLESTICKS
 AVAILABLE_INTERVALS = {'Day': TimeFrame.Day,
                        'Hour': TimeFrame.Hour,
                        'Minute': TimeFrame.Minute,
-                       'Sec': TimeFrame.Sec}
+                       'Sec': TimeFrame.Sec} #Sec is no longer available
 
 
-# THIS ACTUALLY CALLS ALPACA.
-# THIS ACTUALLY CALLS ALPACA.
+# THIS FUNCTION CALLS ALPACA API AND STOCK NEWS API
 def get_alpaca_info(stock, interval, start, end):
   if interval not in AVAILABLE_INTERVALS:
     raise Exception("Interval not supported.")
@@ -29,7 +27,6 @@ def get_alpaca_info(stock, interval, start, end):
 
   return api.get_bars(stock, AVAILABLE_INTERVALS[interval], start, end), news
 
-#my_get_bar("AAPL", 'Day', "2021-06-08", "2021-06-10")
 
 # THIS PARSES THE QUERY PARAMS FROM THE CLIENT.
 # Optional end_date, inject yesterday if not provided.
@@ -38,7 +35,7 @@ def parse_query_params(stock, interval, start_date, end_date= None):
     raise Exception("Missing query params.")
 
   if end_date is None:
-    end_date = str(datetime.date.today() - datetime.timedelta(days=2))
+    end_date = str(datetime.date.today() - datetime.timedelta(days=2)) #2 day time delay because of time zones FIX THIS
 
   return {"stock": stock[0], "interval": interval[0], "start_date": start_date[0], "end_date": end_date}
 
@@ -47,8 +44,6 @@ def view_bars(request):
   parsed_params = parse_query_params(**request.GET)
 
   return HttpResponse(get_alpaca_info(parsed_params['stock'], parsed_params['interval'], parsed_params['start_date'], parsed_params['end_date']))
-
-#view_bars({"stock" : "AAPL", "interval": 'Day', "start_date": "2021-06-08"})
 
 #get_linechart_data: returns an array of timestamps and the vwap value
 def get_linechart_data(bars): #Use this on the output of api.get_bars
@@ -70,9 +65,9 @@ def get_candlestick_data(bars): #Use this on the output of api.get_bars
 
 MOST_RECENT_TIME = str(datetime.datetime.now() - datetime.timedelta(minutes=16))[0:10] + "T" + str(datetime.datetime.now() - datetime.timedelta(minutes=16))[11:19] + "Z"
 
-#print(MOST_RECENT_TIME)
 
 
+#finds "last_week", timeframe in which metrics are pulled from
 def days_offset_fun(RIGHT_NOW):
   market_open = datetime.time(13,31)
 
@@ -91,13 +86,14 @@ my_time = datetime.datetime.now() - datetime.timedelta(minutes=16) - datetime.ti
 
 last_week= str(my_time)[0:10] + "T" + str(my_time)[11:19]+ "Z"
 
-#print(last_week)
 
+#most recent day's close
 def last_day_close_fun(stock):
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   last_day_close= last_week_day_bars[-1].c
   return "Last Day Close", last_day_close
 
+#most recent day's open
 def last_day_open_fun(stock):
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   last_day_open= last_week_day_bars[-1].o
@@ -108,48 +104,53 @@ def last_day_dollar_change_fun(stock):
   last_day_dollar_change = round(last_week_day_bars[-1].c - last_week_day_bars[-1].o, 2)
   return "Last Day Dollar Change", last_day_dollar_change
 
+#most recent day's close minus open as a percentage of most recent day's open
 def last_day_perc_change_fun(stock):
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   last_day_dollar_change = last_week_day_bars[-1].c - last_week_day_bars[-1].o
   last_day_perc_change = (last_day_dollar_change / last_week_day_bars[-1].o) * 100
   return "Last Day Percent Change", last_day_perc_change
 
+#most recent day's volume
 def last_day_volume_fun(stock):
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   last_day_volume = last_week_day_bars[-1].v
   return "Last Day Volume", last_day_volume
 
+#most recent days's low
 def last_day_low_fun(stock):
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   last_day_low = last_week_day_bars[-1].l
   return "Last Day Low", last_day_low
 
+#most recent day's high
 def last_day_high_fun(stock):
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   last_day_high = last_week_day_bars[-1].h
   return "Last Day High", last_day_high
 
+
+#ONE YEAR AGO DATETIME FOR ONE_YEAR_AGO FUNCTIONS
 one_year_ago_today = datetime.datetime.now() - datetime.timedelta(days=365)
 
 one_year_ago_today_str = str(one_year_ago_today - datetime.timedelta(minutes=16))[0:10] + "T" + str(one_year_ago_today - datetime.timedelta(minutes=16))[11:19] + "Z"
 
-#print(one_year_ago_today)
 
+#"ALMOST" ONE YEAR DATA NO LONGER USED - DELETE NEXT TIME YOU HAVE VIEWS RUNNING
 almost_one_year_ago_today = datetime.datetime.now() - datetime.timedelta(days=364)
 
 almost_one_year_ago_today_str = str(almost_one_year_ago_today - datetime.timedelta(minutes=16))[0:10] + "T" + str(almost_one_year_ago_today - datetime.timedelta(minutes=16))[11:19] + "Z"
 
-#print(almost_one_year_ago_today)
-
-
-def one_year_change_fun(stock): 
+#change in price over one year as a percentage of one_year_ago today's close
+def one_year_change_fun(stock):
   one_year_ago_today_bars = api.get_bars(stock, TimeFrame.Day, one_year_ago_today_str, MOST_RECENT_TIME)
   one_year_ago_today_close = one_year_ago_today_bars[0].c
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   last_day_close= last_week_day_bars[-1].c
   one_year_change_per = (last_day_close - one_year_ago_today_close) / one_year_ago_today_close * 100
   return "One Year Percent Change", one_year_change_per
-  
+
+#highest price over last year
 def one_year_high_fun(stock):
   one_years_bars = api.get_bars(stock, TimeFrame.Day, one_year_ago_today_str, MOST_RECENT_TIME)
   one_years_highs = []
@@ -158,6 +159,7 @@ def one_year_high_fun(stock):
   one_year_high = max(one_years_highs)
   return "52 Week High", one_year_high
 
+#lowest price over past year
 def one_year_low_fun(stock):
   one_years_bars = api.get_bars(stock, TimeFrame.Day, one_year_ago_today_str, MOST_RECENT_TIME)
   one_years_lows = []
@@ -166,6 +168,7 @@ def one_year_low_fun(stock):
   one_year_low = min(one_years_lows)
   return "52 Week Low", one_year_low
 
+#filters list of stocks based on most recent day's volume
 def filter_volume(stocks):
   stockDict = {}
   for x in stocks:
@@ -173,6 +176,7 @@ def filter_volume(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+#filters list of stocks based on most recent day's close
 def filter_close(stocks):
   stockDict = {}
   for x in stocks:
@@ -180,6 +184,7 @@ def filter_close(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+#filters list of stocks based on most recent day's open
 def filter_open(stocks):
   stockDict = {}
   for x in stocks:
@@ -187,6 +192,7 @@ def filter_open(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+ #filters list of stocks based on most recent day's change in dollar price
 def filter_dollar_change(stocks):
   stockDict = {}
   for x in stocks:
@@ -194,6 +200,7 @@ def filter_dollar_change(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+ #filters list of stocks based on most recent day's change in percentage based price
 def filter_perc_change(stocks):
   stockDict = {}
   for x in stocks:
@@ -201,6 +208,7 @@ def filter_perc_change(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+ #filters list of stocks based on most recent day's low
 def filter_low(stocks):
   stockDict = {}
   for x in stocks:
@@ -208,6 +216,7 @@ def filter_low(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+ #filters list of stocks based on most recent day's high
 def filter_high(stocks):
   stockDict = {}
   for x in stocks:
@@ -215,6 +224,7 @@ def filter_high(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+ #filters list of stocks based on last year's change in percentage based price
 def filter_one_year_change(stocks):
   stockDict = {}
   for x in stocks:
@@ -222,6 +232,7 @@ def filter_one_year_change(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+#filters list of stocks based on last year's high
 def filter_one_year_high(stocks):
   stockDict = {}
   for x in stocks:
@@ -229,6 +240,7 @@ def filter_one_year_high(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+#filters list of stocks based on last year's low
 def filter_one_year_low(stocks):
   stockDict = {}
   for x in stocks:
@@ -236,6 +248,7 @@ def filter_one_year_low(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+#filters list of stocks based on last day's change in dollar based price change
 def filter_daysRange(stocks):
   stockDict = {}
   for x in stocks:
@@ -243,6 +256,8 @@ def filter_daysRange(stocks):
   sort_stocks = sorted(stockDict.items(), key=lambda x: x[1], reverse=True)
   return sort_stocks
 
+
+#returns data from last five trading days
 def five_days(stock):
   last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_week, MOST_RECENT_TIME)
   le = len(last_week_day_bars)
@@ -251,15 +266,3 @@ def five_days(stock):
   else:
        last_week_day_bars = api.get_bars(stock, TimeFrame.Day, last_2weeks, MOST_RECENT_TIME)
        return (last_week_day_bars)[(le - 5):le]
-
-#print(last_day_close_fun("AAPL"))
-#print(last_day_open_fun("AAPL"))
-#print(last_day_dollar_change_fun("AAPL"))
-#print(last_day_perc_change_fun("AAPL"))
-#print(last_day_volume_fun("AAPL"))
-#print(last_day_low_fun("AAPL"))
-#print(last_day_high_fun("AAPL"))
-
-#print(one_year_change_fun("AAPL"))
-#print(one_year_high_fun("AAPL"))
-#print(one_year_low_fun("AAPL"))
